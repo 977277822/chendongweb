352103a0580ad816d2fe129d2dd058859d9a737a
lzf <1065500519@qq.com>
2015-08-17 22:10:31 +0800

    ttt

--- web/src/main/java/com/Test.java
+++ web/src/main/java/com/Test.java
@@ -1,5 +1,15 @@
 package com;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
 public class Test {
 
+	@RequestMapping("add")
+	public void add(Integer id , String uname) {
+		System.out.println(id + "--->" + uname);
+	}
+	
 }
