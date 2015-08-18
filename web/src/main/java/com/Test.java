f6d135022f72aa9e5f02b5470367dec7ac0f5e66
977277822 <977277822@qq.com>
2015-08-18 22:29:31 +0800

    ttt

--- web/src/main/java/com/Test.java
+++ web/src/main/java/com/Test.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("test")
-public class Test {
+public class Test{
 
 	@RequestMapping("add")
 	public void add(Integer id , String uname) {
