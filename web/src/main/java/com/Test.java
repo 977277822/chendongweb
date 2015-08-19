48c8da44076c002201cb0a7a5f4e0528dd7e3deb
977277822 <977277822@qq.com>
2015-08-19 22:44:52 +0800

    ttt

--- web/src/main/java/com/Test.java
+++ web/src/main/java/com/Test.java
@@ -1,15 +1,26 @@
 package com;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("test")
-public class Test{
-
+public class Test {
+	
 	@RequestMapping("add")
-	public void add(Integer id , String uname) {
+	public void add(Integer id, String uname) {
+		Configuration config = new Configuration().configure("/hibernate/hibernate.cfg.xml");;
+		SessionFactory sf = config.buildSessionFactory(new ServiceRegistryBuilder().configure().buildServiceRegistry());
+		Session session = sf.openSession();
+		MainTest t = new MainTest();
+		t.setId(1);
+		t.setUname("ddd");
+		session.save(t);
 		System.out.println(id + "--->" + uname);
 	}
-	
+
 }
