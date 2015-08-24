a33443333afceb9f0e501f8e4788f62210c0173c
977277822 <977277822@qq.com>
2015-08-24 21:41:29 +0800

    tttt

---
+++ web-service/src/main/java/com/dongchen/entity/UserTest.java
@@ -0,0 +1,45 @@
+package com.dongchen.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test")
+public class UserTest implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+	private int id;
+	
+	@Column(name="uname")
+	private String uname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	
+	
+
+}
