c942e64c2a0f7f8ac63fef45306b097db3f8590a
977277822 <977277822@qq.com>
2015-08-24 23:05:27 +0800

    ttt

---
+++ web-service/src/main/java/com/dongchen/test/Test.java
@@ -0,0 +1,19 @@
+package com.dongchen.test;
+
+import org.hibernate.Session;
+
+import com.dongchen.entity.UserTest;
+import com.dongchen.util.ClassLoaderConfig;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+//		Session session  = ClassLoaderConfig.getSession();
+//		
+//		UserTest t = new UserTest();
+//		t.setUname("zzz");
+//		
+//		session.save(t);
+	}
+}
