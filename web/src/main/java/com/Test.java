752db5fabe51e84be71943ed2d792ef1aede3288
977277822 <977277822@qq.com>
2015-08-25 22:30:57 +0800

    ttt

--- web/src/main/java/com/Test.java
+++ web/src/main/java/com/Test.java
@@ -1,27 +1,28 @@
 package com;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dongchen.entity.UserTest;
+import com.dongchen.util.ClassLoaderConfig;
 
 @Controller
 @RequestMapping("test")
 public class Test {
-	
+
 	@RequestMapping("add")
 	public void add(Integer id, String uname) {
-		
-	}
-	
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/application-config.xml");
-		SessionFactory sessionFactory = ctx.getBean("sessionFactory", SessionFactory.class);
-		Session session = sessionFactory.openSession();
-		MainTest t = new MainTest();
-		t.setUname("1212121212121");
+		Session session = ClassLoaderConfig.getSession();
+
+		UserTest t = new UserTest();
+		t.setUname("zzz");
+
 		session.save(t);
+	}
+
+	public static void main(String[] args) {
+		
 	}
 
 }
